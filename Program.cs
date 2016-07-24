e1bbe0b30e5b2b1886e7450b42c624888a7b3e86
Junle Li <lijunle@gmail.com>
2016-07-24 23:32:02 +0800

    New a C# .Net core project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Trello.Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
